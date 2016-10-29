4bd6f50abef68f4214bf0ca2e1b47cad7797d06d
Geo <george.gripca@gmail.com>
2016-10-29 20:34:57 +0300

    commit UltitarTranzit final impl

--- UtilitarTranzit.py
+++ UtilitarTranzit.py
@@ -1,6 +1,3 @@
-from math import fabs
-
-
 def obtine_culoare_dupa_sens(sens: int):
     if sens > 0:
         return 'A'
@@ -12,7 +9,7 @@ def este_pe_tabla(linie: int, coloana: int):
 
 
 def mutare_valida_inainte(tabla: list, ln_act: int, ln_urm: int, col: int, sens: int):
-    return tabla[ln_urm][col] == 0 and tabla[ln_act + sens][col] == 0
+    return tabla[ln_urm][col] == '0' and tabla[ln_act + sens][col] == '0'
 
 
 def mutare_valida_normala_in_diag(tabla: list, ln_urm: int, col_urm: int, sens: int):
@@ -26,10 +23,10 @@ def mutare_en_passant_valida(tabla_prec: list, tabla_act: list, ln_act: int, ln_
     col_prec_piesa_adv = col_urm
     ln_act_piesa_adv = ln_act
     col_act_piesa_adv = col_urm
-    return (tabla_act[ln_urm][col_urm] == 0
+    return (tabla_act[ln_urm][col_urm] == '0'
             and tabla_prec[ln_prec_piesa_adv][col_prec_piesa_adv] == culoare_adv
-            and tabla_prec[ln_act_piesa_adv][col_act_piesa_adv] == 0
-            and tabla_prec[ln_prec_piesa_adv][col_prec_piesa_adv] == 0
+            and tabla_prec[ln_act_piesa_adv][col_act_piesa_adv] == '0'
+            and tabla_prec[ln_prec_piesa_adv][col_prec_piesa_adv] == '0'
             and tabla_act[ln_act_piesa_adv][col_act_piesa_adv] == culoare_adv)
 
 
@@ -40,12 +37,12 @@ def mutare_valida_in_diag(tabla_prec: list, tabla_act: list, ln_act: int, ln_urm
 
 def mutare_valida_in_diag_jucator(tabla_prec: list, tabla_act: list, ln_act: int, col_act: int, ln_urm: int,
                                   col_urm: int, sens: int):
-    return ln_urm == ln_act + sens and fabs(col_urm - col_act) == 1 \
+    return ln_urm == ln_act + sens and abs(col_urm - col_act) == 1 \
            and mutare_valida_in_diag(tabla_prec, tabla_act, ln_act, ln_urm, col_urm, sens)
 
 
 def mutare_valida_inainte_jucator(tabla: list, ln_act: int, col_act: int, ln_urm: int, col_urm, sens: int):
-    return col_act == col_urm and 0 < fabs(ln_urm - ln_act) * sens < 3 \
+    return col_act == col_urm and 0 < abs(ln_urm - ln_act) * sens < 3 \
            and mutare_valida_inainte(tabla, ln_act, ln_urm, col_act, sens)
 
 
